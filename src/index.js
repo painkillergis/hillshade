c8f88b7131b8626b9274058b5abd034532f2c0d8
arctair <jtyler_@outlook.com>
2020-02-22 11:37:51 -0600

    failing acceptance test: should return shaded relief TIFF with predetermined extent

--- src/index.js
+++ src/index.js
@@ -6,17 +6,34 @@ typeof describe === 'undefined' || describe('service', function () {
   const request = require('request-promise');
   chai.should();
   chai.use(require('chai-as-promised'));
-  it('should return shaded relief TIFF with predetermined size and extent', async function () {
+  it('should return shaded relief TIFF with predetermined extent', async function () {
     const sha1 = buffer => crypto.createHash('sha1')
       .update(buffer)
       .digest('hex');
-    const response = await request({
+
+    const responseBig = await request({
+      json: {
+        width: 256,
+        height: 256,
+      },
       encoding: null,
       resolveWithFullResponse: true,
       uri: 'http://localhost:8080',
     });
-    response.headers['content-type'].should.contain('image/tiff');
-    sha1(response.body).should.equal(sha1(fs.readFileSync('./src/shaded-relief.tif')));
+    responseBig.headers['content-type'].should.contain('image/tiff');
+    sha1(responseBig.body).should.equal(sha1(fs.readFileSync('./src/shaded-relief.tif')));
+
+    const responseSmall = await request({
+      json: {
+        width: 128,
+        height: 128,
+      },
+      encoding: null,
+      resolveWithFullResponse: true,
+      uri: 'http://localhost:8080',
+    });
+    responseSmall.headers['content-type'].should.contain('image/tiff');
+    sha1(responseSmall.body).should.equal(sha1(fs.readFileSync('./src/shaded-relief-small.tif')));
   });
   after(function () {
     server.close();
