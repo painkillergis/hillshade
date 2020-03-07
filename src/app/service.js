b1b7c69a491f7d77112b3a36d8de28505b2acd14
arctair <jtyler_@outlook.com>
2020-03-07 04:31:45 -0600

    unit tests vs acceptance tests

--- src/app/service.js
+++ src/app/service.js
@@ -11,8 +11,8 @@ const render = async ({
   const upperLefts = bounds.toUpperLefts(extent);
   const imgPaths = img.pathsFromUpperLefts(upperLefts);
   cp.execSync(`gdalbuildvrt -overwrite /tmp/elevation.vrt ${imgPaths.join(' ')}`);
-  cp.execSync(`python src/translate.py /tmp/elevation.vrt /tmp/translate.tif ${width} ${height} ${left} ${top} ${right} ${bottom}`, { stdio: 'inherit' });
-  cp.execSync(`blender -b -P src/render.py -noaudio -o ///tmp/shaded-relief-#.tif -f 0 -- ${width} ${height} 2.0`, { stdio: 'inherit' });
+  cp.execSync(`python src/app/translate.py /tmp/elevation.vrt /tmp/translate.tif ${width} ${height} ${left} ${top} ${right} ${bottom}`, { stdio: 'inherit' });
+  cp.execSync(`blender -b -P src/app/render.py -noaudio -o ///tmp/shaded-relief-#.tif -f 0 -- ${width} ${height} 2.0`, { stdio: 'inherit' });
   return fs.readFileSync('/tmp/shaded-relief-0.tif');
 };
 
