c40fbf3c3f4bdffb8cb772ae82ea8b54f13a1c7a
arctair <jtyler_@outlook.com>
2020-03-21 06:54:16 -0500

    bounds uses geojsonToUpperLefts.py instead of half-assed js implementation

--- src/app/bounds.js
+++ src/app/bounds.js
@@ -1,5 +1,48 @@
 typeof describe === 'undefined' || describe('bounds', function () {
   require('chai').should();
+  describe('featureCollectionToUpperLefts', function () {
+    it('should include an upper left', async function () {
+      const featureCollection = {
+        type: 'FeatureCollection',
+        name: 'cutline',
+        crs: {
+          type: 'name',
+          properties: {
+            name: 'urn:ogc:def:crs:OGC:1.3:CRS84'
+          }
+        },
+        features: [{
+          type: 'Feature',
+          properties: {
+            id: null
+          },
+          geometry: {
+            type: 'MultiPolygon',
+            coordinates: [[[
+              [-98.90216954661751,
+                47.7956910847549],
+              [-98.78680134632724,
+                47.794844603763806],
+              [-98.76910569067988,
+                47.77466954033169],
+              [-98.79230282819937,
+                47.7461652095241],
+              [-98.90235068056387,
+                47.74869588814863],
+              [-98.93943909892864,
+                47.776015190720294],
+              [-98.90216954661751,
+                47.7956910847549]
+            ]]]
+          }
+        }]
+      };
+
+      await featureCollectionToUpperLefts(featureCollection).should.eventually.deep.equal([
+        { lat: 48, lon: -99 },
+      ]);
+    });
+  });
   describe('toUpperLefts', function () {
     it('should include an upper left', function () {
       toUpperLefts({
@@ -41,6 +84,20 @@ typeof describe === 'undefined' || describe('bounds', function () {
     });
   });
 });
+
+const spawnSync = require('child_process').spawnSync;
+
+const featureCollectionToUpperLefts = async featureCollection => {
+  const { stdout } = spawnSync(
+    'python',
+    ['src/app/geojsonToUpperLefts.py'],
+    {
+      input: JSON.stringify(featureCollection),
+      encoding: 'utf8'
+    },
+  );
+  return JSON.parse(stdout);
+}
 
 const toUpperLefts = ({ left, top, right, bottom }) => {
   const upperLefts = [];
