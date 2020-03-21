d3ef10c7f7dff4a25dae0e5557363dd815e17326
arctair <jtyler_@outlook.com>
2020-03-21 13:49:11 -0500

    fix blender stdout overflow

--- src/app/blender.js
+++ src/app/blender.js
@@ -1,25 +1,34 @@
-const promisify = require('util').promisify
-const exec = promisify(require('child_process').exec);
+const spawn = require('child_process').spawn;
 
-const renderShadedRelief = async ({
+const renderShadedRelief = ({
   id,
   size: {
     width,
     height,
   },
   scale,
-}) => exec([
-  'blender',
-  '-b',
-  '-P src/app/blender.py',
-  '-noaudio',
-  `-o ///tmp/${id}-#.tif`,
-  '-f 0',
-  '--',
-  `/tmp/${id}-heightmap.tif`,
-  width,
-  height,
-  scale,
-].join(' '));
+}) => {
+  const child = spawn(
+    'blender',
+    [
+      '-b',
+      '-P', 'src/app/blender.py',
+      '-noaudio',
+      '-o', `///tmp/${id}-#.tif`,
+      '-f', '0',
+      '--',
+      `/tmp/${id}-heightmap.tif`,
+      width,
+      height,
+      scale,
+    ]
+  );
+  const stderr = [];
+  child.stderr.setEncoding('utf8');
+  child.stderr.on('data', data => stderr.push(data));
+  return new Promise((resolve, reject) => {
+    child.addListener('exit', code => code === 0 ? resolve() : reject(stderr.join('')));
+  });
+}
 
 module.exports = { renderShadedRelief };
