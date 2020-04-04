3f565b865152be47d60bf1fa1250249816f6f3e8
arctair <jtyler_@outlook.com>
2020-04-04 13:19:07 -0500

    obsolete script

--- installAssets.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -f 'assets/USGS_NED_13_n38w106_IMG.zip' ]; then
-  aws s3 cp s3://prd-tnm/StagedProducts/Elevation/13/IMG/USGS_NED_13_n38w106_IMG.zip assets/
-  unzip -d assets/ assets/USGS_NED_13_n38w106_IMG.zip USGS_NED_13_n38w106_IMG.img
-fi
-if [ ! -f 'assets/n38w107.zip' ]; then
-  aws s3 cp s3://prd-tnm/StagedProducts/Elevation/13/IMG/n38w107.zip assets/
-  unzip -d assets/ assets/n38w107.zip imgn38w107_13.img
-fi
