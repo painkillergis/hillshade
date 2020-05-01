179010ac7eddde72eb613ad92dde95eaca5b996b
arctair <jtyler_@outlook.com>
2020-05-01 13:04:22 -0500

    delete everything but python scripts

--- src/app/virtualDataset.js
+++
@@ -1,5 +0,0 @@
-const exec = require('util').promisify(require('child_process').exec);
-
-const build = ({ imgPaths, destination }) => exec(`gdalbuildvrt -overwrite ${destination} ${imgPaths.join(' ')}`);
-
-module.exports = { build };
