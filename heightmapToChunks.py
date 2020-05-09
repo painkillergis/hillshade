97564eaf28cf3722169809903d0a04025f5baa5c
arctair <jtyler_@outlook.com>
2020-05-09 16:26:07 -0500

    heightmapToChunks don't clamp sides of chunks to sides of original heightmap

--- heightmapToChunks.py
+++ heightmapToChunks.py
@@ -12,9 +12,9 @@ heightmap = Image.open(args.heightmap)
 width, height = heightmap.size
 for y in range(0, int(height / args.chunkHeight)):
   for x in range(0, int(width / args.chunkWidth)):
-    left = max((x - 1) * args.chunkWidth, 0)
-    right = min((x + 2) * args.chunkWidth, width)
-    top = max((y - 1) * args.chunkHeight, 0)
-    bottom = min((y + 2) * args.chunkHeight, height)
+    left = (x - 1) * args.chunkWidth
+    right = (x + 2) * args.chunkWidth
+    top = (y - 1) * args.chunkHeight
+    bottom = (y + 2) * args.chunkHeight
     chunk = heightmap.crop((left, top, right, bottom))
     chunk.save(f'{args.destinationDirectory}/{y}-{x}.tif')
