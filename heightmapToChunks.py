fff5f7e8ab1aedc350d43488a4419b3781181c4f
arctair <jtyler_@outlook.com>
2020-05-09 16:34:24 -0500

    nitpicking about python style

--- heightmapToChunks.py
+++ heightmapToChunks.py
@@ -12,9 +12,11 @@ heightmap = Image.open(args.heightmap)
 width, height = heightmap.size
 for y in range(0, int(height / args.chunkHeight)):
   for x in range(0, int(width / args.chunkWidth)):
-    left = (x - 1) * args.chunkWidth
-    right = (x + 2) * args.chunkWidth
-    top = (y - 1) * args.chunkHeight
-    bottom = (y + 2) * args.chunkHeight
-    chunk = heightmap.crop((left, top, right, bottom))
-    chunk.save(f'{args.destinationDirectory}/{y}-{x}.tif')
+    heightmap \
+      .crop((
+        (x - 1) * args.chunkWidth,
+        (y - 1) * args.chunkHeight,
+        (x + 2) * args.chunkWidth,
+        (y + 2) * args.chunkHeight,
+      )) \
+      .save(f'{args.destinationDirectory}/{y}-{x}.tif')
