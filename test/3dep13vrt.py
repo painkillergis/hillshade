d785986c75dcf2d2b9719ae43a4842b74dd736b8
arctair <jtyler_@outlook.com>
2020-05-25 11:55:59 -0500

    intersecting 3dep ids from cutline handles non-north-western hemisphere

--- test/3dep13vrt.py
+++ test/3dep13vrt.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 
 
 def test_should_build_vrt_from_cutline(tmpdir):
-  vrt('test/cutline.json', 'test/3dep13.d', f'{tmpdir}/actual.vrt')
+  vrt('test/cutline-seattle.json', 'test/3dep13.d', f'{tmpdir}/actual.vrt')
   with open(f'{tmpdir}/actual.vrt', 'r') as file:
     actualVrt = file.read()
   with open('test/expected.vrt', 'r') as file:
@@ -24,7 +24,18 @@ def vrt(cutlineFile, threeDepDirectory, destinationFile):
 
 
 def test_intersecting_3dep_ids_from_cutline():
-  assert intersecting3DepIdsFromCutline('test/cutline.json') == ['n49w124', 'n48w123', 'n49w123']
+  assert intersecting3DepIdsFromCutline('test/cutline-seattle.json') == ['n49w124', 'n48w123', 'n49w123']
+  assert intersecting3DepIdsFromCutline('test/cutline-0-0.json') == [
+    's01w001',
+    'n00w001',
+    'n01w001',
+    's01e000',
+    'n00e000',
+    'n01e000',
+    's01e001',
+    'n00e001',
+    'n01e001',
+  ]
 
 
 def intersecting3DepIdsFromCutline(cutlineFile):
@@ -48,4 +59,18 @@ def intersecting3DepIdsFromCutline(cutlineFile):
   upperLefts = [(int(-180 + x), int(90 - y)) for x, y in zip(xIndices, yIndices)]
   upperLefts = sorted(upperLefts, key=lambda xy: xy[1])
   upperLefts = sorted(upperLefts, key=lambda xy: xy[0])
-  return [f'n{y}w{abs(x)}' for x, y in upperLefts]
+  return list(map(upperLeftTo3DepId, upperLefts))
+
+
+def test_upper_left_to_3dep_id():
+  assert upperLeftTo3DepId((-100, 10)) == 'n10w100'
+  assert upperLeftTo3DepId((-10, 10)) == 'n10w010'
+  assert upperLeftTo3DepId((-10, 1)) == 'n01w010'
+  assert upperLeftTo3DepId((10, 1)) == 'n01e010'
+  assert upperLeftTo3DepId((10, -1)) == 's01e010'
+  assert upperLeftTo3DepId((0, 0)) == 'n00e000'
+
+
+def upperLeftTo3DepId(lonLat):
+  x, y = lonLat
+  return f'{"s" if y < 0 else "n"}{abs(y):02}{"w" if x < 0 else "e"}{abs(x):03}'
